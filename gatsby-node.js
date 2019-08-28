94bac53a610e7e89f0390adb2e0adfee5ab0872c
Alina Garipova <alina.garipova@headmade.pro>
2019-08-28 18:33:56 +0300

    Create pages about projects from data from a md files

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,29 @@
  */
 
 // You can delete this file if you're not using it
+const path = require("path")
+
+exports.createPages = async function({ actions, graphql, reporter }) {
+    const { data } = await graphql(`
+    {
+      allMarkdownRemark(sort: {fields: [frontmatter___order], order: DESC}) {
+        edges {
+          node {
+            frontmatter {
+               path
+            }
+          }
+        }
+      }
+}
+
+  `)
+    data.allMarkdownRemark.edges.forEach(edge => {
+        const url = edge.node.frontmatter.path
+        actions.createPage({
+            path: url,
+            component: path.resolve(`./src/templates/projectTemplate.js`),
+            context: { slug: url },
+        })
+    })
+}
--- src/templates/projectTemplate.js
+++ src/templates/projectTemplate.js
@@ -1,46 +1,74 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import { graphql, Link} from 'gatsby'
 import Layout from '../components/layout'
 
-import pic11 from '../assets/images/pic11.jpg'
+import pic09 from '../assets/images/pic09.jpg'
 
-const Generic = (props) => (
+export const query = graphql`
+  query ProjectQuery($path: String!) {
+      markdownRemark(frontmatter: { path: { eq: $path } }) {
+          frontmatter {
+              path
+              title
+              description
+              participation
+              href
+              statistics
+              technologies
+              longDesktopImg
+              desktopImg
+              tabletImg
+              files
+              phoneImg
+          }
+      }
+  }
+
+`;
+
+function Generic ({data}){
+    const frontmatter = data.markdownRemark.frontmatter
+    console.log(frontmatter)
+    return (
     <Layout>
         <Helmet>
             <title>Genesis - О нас</title>
             <meta name="description" content="Generic Page" />
         </Helmet>
-
         <section id="two" className="spotlights">
-            {/*{edges.map(edge => {*/}
-                {/*const projects = edge.node.frontmatter*/}
-                // return (
-                    <section>
-                        <Link to="/generic" className="image">
-                            <img src={pic08} alt=""/>
-                        </Link>
-                        <div className="content">
-                            <div className="inner">
-                                <header className="major">
-                                    <h3>title</h3>
-                                </header>
-                                <p>description</p>
-                                <h4>Технологии:</h4>
-                                <ul>
-                                    {/*{projects.technologies.map(technology => {*/}
-                                        {/*return(*/}
-                                            <li>технологии</li>
-                                    //     )
-                                    // })}
-                                </ul>
-                            </div>
-                        </div>
-                    </section>
-            //     )
-            // })}
+            <section>
+                <Link to="/generic" className="image">
+                    <img src={pic09} alt=""/>
+                </Link>
+                <div className="content">
+                    <div className="inner">
+                        <header className="major">
+                            <h3>{frontmatter.title}</h3>
+                        </header>
+                        <h4>Описание:</h4>
+                        <p>{frontmatter.description} <br/><a href={frontmatter.href} target="_blank">{frontmatter.href}</a></p>
+                        <h4>Наше участие:</h4>
+                        <ul>
+                            {frontmatter.participation.map(element => {
+                                return (
+                                    <li>{element}</li>
+                                )
+                            })}
+                        </ul>
+                        <h4>Показатели:</h4>
+                        <ul>
+                            {frontmatter.statistics.map(element => {
+                                return (
+                                    <li>{element}</li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            </section>
         </section>
-
     </Layout>
-)
+)}
 
 export default Generic
