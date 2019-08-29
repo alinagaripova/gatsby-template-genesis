e80dfdaa09abb38f5269c346e85761c51496cccb
Alina Garipova <alina.garipova@headmade.pro>
2019-08-29 13:08:26 +0300

    Create new directory articles and md file here

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,19 +13,26 @@ module.exports = {
               name: `src`,
               path: `${__dirname}/src/projects`,
           },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
       },
-    },
+      {
+          resolve: `gatsby-source-filesystem`,
+          options: {
+              name: `src`,
+              path: `${__dirname}/src/articles`,
+          },
+      },
+      {
+      resolve: `gatsby-plugin-manifest`,
+          options: {
+            name: 'gatsby-starter-default',
+            short_name: 'starter',
+            start_url: '/',
+            background_color: '#663399',
+            theme_color: '#663399',
+            display: 'minimal-ui',
+            icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
+          },
+      },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline'
   ],
