9885981ac1870a3330241fe28a0d3f1fe51f093e
Alina Garipova <alina.garipova@headmade.pro>
2019-09-03 10:49:11 +0300

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -60,37 +60,5 @@ exports.createPages = ({ actions, graphql }) => {
             })
         })
     });
-    return Promise.all([projectsData]);
+    return Promise.all([projectsData, articlesData]);
 }
-// data.allMarkdownRemark.edges.forEach(edge => {
-//         const url = edge.node.frontmatter.path
-//         actions.createPage({
-//             path: url,
-//             component: path.resolve(`./src/templates/projectTemplate.js`),
-//             context: { slug: url },
-//         })
-//     })
-// }
-// exports.createPages = async function({ actions, graphql, reporter }) {
-//     const { data } = await graphql(`
-//     {
-//       allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }, filter: {frontmatter: {name: {eq: "article"}}}) {
-//         edges {
-//           node {
-//             frontmatter {
-//                path
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `)
-//     data.allMarkdownRemark.edges.forEach(edge => {
-//         const url = edge.node.frontmatter.path
-//         actions.createPage({
-//             path: url,
-//             component: path.resolve(`./src/templates/articleTemplate.js`),
-//             context: { slug: url },
-//         })
-//     })
-// }
--- src/templates/articleTemplate.js
+++ src/templates/articleTemplate.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import { graphql, Link} from 'gatsby'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 
 import pic11 from '../assets/images/pic11.jpg'
